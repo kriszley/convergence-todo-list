238b8aae0047d1457a8450133014ab0cb2b235cf
Kris Lee <kriszley>
2020-11-17 02:21:11 -0800

    Complete skeleton for services

---
+++ src/services/auth.js
@@ -0,0 +1,22 @@
+// Function to check if user exists in the DB
+function check_user_exist(email) {
+    // TODO: Call check_user() from Models that returns `user obj`
+
+    return /* True/False */
+}
+
+// Function to validate password for the given user_id
+function check_passwords(user_id, password) {
+    // TODO: Call get_password() from Models that returns `password`
+
+    // TODO: Compare passwords
+
+    return /* True/False */
+}
+
+// Function to return a generated token.
+function generate_auth_token(user_id, item_id, data) {
+    // TODO: Call update_item() from Models that returns `item_id`
+
+    return /* token */
+}
---
+++ src/services/item.js
@@ -0,0 +1,34 @@
+// Function to return an item given an item id.
+function read_item(item_id) {
+    // TODO: Call read_item() from Models that returns `item_obj`
+
+    return /* item obj */
+}
+
+// Function to create and return an item id
+function create_item(user_id, data) {
+    // TODO: Call create_item() from Models that returns `item_id`
+
+    return /* item id */
+}
+
+// Function to update and return an item id
+function update_item(user_id, item_id, data) {
+    // TODO: Call update_item() from Models that returns `item_id`
+
+    return /* item id */
+}
+
+// Function to delete and return an item id
+function delete_item(user_id, data) {
+    // TODO: Call delete_item() from Models that returns `item_id`
+
+    return /* item id */
+}
+
+// Function to search and return a list of item objects
+function search_item(item_id, data) {
+    // TODO: Call search_item() from Models that returns list of item objects
+
+    return /* list of item objects */
+}
